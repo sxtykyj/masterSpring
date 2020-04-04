6e31c9ae5d38150f5c82b321aa6400b7805c655a
sxtykyj <yujie.kang0925@hotmail.com>
2020-04-04 12:51:23 -0400

    日志学习_logback + slf4j

---
+++ code/LogStudy/slf4j_demo/src/main/java/com/smart/testSlf4j.java
@@ -0,0 +1,38 @@
+package com.smart;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Author: yk
+ * @Date: 2020/4/4 11:42
+ */
+public class testSlf4j {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(testSlf4j.class);
+
+    // 快速入门
+    @Test
+    public void test() throws Exception {
+        // 日志输出 (默认级别为info)
+        LOGGER.error("error");
+        LOGGER.warn("warn");
+        LOGGER.info("info");
+        LOGGER.debug("debug");
+        LOGGER.trace("trace");
+
+        // 使用占位符输出日志信息
+        String name = "smart";
+        Integer age = 14;
+        LOGGER.info("用户：{}，{}", name, age);
+
+        // 将系统的异常信息输出
+        try {
+            int i = 1 / 0;
+        } catch (Exception e) {
+            LOGGER.error("出现异常：", e);
+        }
+
+    }
+}
